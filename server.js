087d84fc3eb8d4f67c6fb305e62ee00ca16da91f
Olaoluwa99 <olaoluwadaniel99@gmail.com>
2025-10-26 15:45:26 +0100

    Crash fix V5

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 const { PORT } = require('./src/config/constants');
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
